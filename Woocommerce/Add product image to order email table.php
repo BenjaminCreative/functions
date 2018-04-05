a43b794dba0f8239488ce7595dabdefa5d30cf72
Lyder <martinlyder@gmail.com>
2018-04-05 15:42:50 +0200

    woocommerce and one in the misc!

---
+++ Woocommerce/Add product image to order email table.php
@@ -0,0 +1,16 @@
+/**
+ * Adds product images to the WooCommerce order emails table
+ * Uses WooCommerce 2.5 or newer
+ */
+add_filter( 'woocommerce_email_order_items_args', 'iconic_email_order_items_args', 10, 1 );
+
+function iconic_email_order_items_args( $args ) {
+
+    $args['show_image'] = true;
+    $args['image_size'] = array( 100, 100 );
+    $args['show_purchase_note'] = true;
+    $args['Card type'] = true;
+
+    return $args;
+
+}
